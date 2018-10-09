2b756ce06104b90f62175b66bebd4b39068c856e
Shahriar <ssha7332@gmail.com>
2018-10-09 12:20:47 -0600

    Added Model Group and renamed File.

--- Firstios/Controller/ColorChangeViewController.swift
+++ Firstios/Controller/ColorChangeViewController.swift
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+public class ColorChangeViewController: UIViewController
+{
+    public override func viewDidLoad() -> Void
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
---
+++ Firstios/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  Firstios
+//
+//  Created by Shams, Shahriar on 10/9/18.
+//  Copyright © 2018 Ctec. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
